4e893ffbf415f332eb60c6b05ac8ecdc8e3979aa
emanoeljunior21 <emanoeljuniordev@gmail.com>
2024-07-08 12:06:18 -0300

    project drawer

---
+++ script.js
@@ -0,0 +1,8 @@
+function generateNumber() {
+  const min = Math.ceil(document.querySelector(".input-min").value);
+  const max = Math.floor(document.querySelector(".input-max").value);
+
+  const result = Math.floor(Math.random() * (max - min) + min);
+
+  alert(result);
+}
